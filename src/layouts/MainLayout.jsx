ba320e6ac9b1337b66c5d22e2c3cf26bb144f28f
Tarif24 <tarif24@hotmail.com>
2025-04-17 22:23:34 -0400

    added react router and tailwindcss

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1>MainLayout</h1>
+            <div>
+                <Outlet />
+            </div>
+        </div>
+    );
+};
+
+export default MainLayout;
